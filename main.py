3b503ecfb4e25926f05ef22a0a94d812b2e0ff83
VolosnjovFA <feodor.a.v.03@gamil.com>
2025-01-28 13:48:58 +0300

    changing the file structure

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+from webchat.websockets import router as websockets_router
 
 app = FastAPI()
-
-active_connections = []
+app.include_router(websockets_router)
 
 html = """
 <!DOCTYPE html>
@@ -49,47 +49,9 @@ html = """
 </html>
 """
 
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str, author: WebSocket):
-        for connection in self.active_connections:
-            if connection != author:
-                await connection.send_text(message)
-
-
-manager = ConnectionManager()
-
 @app.get("/")
 async def get():
     return HTMLResponse(html)
-
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You: {data}", websocket)
-            await manager.broadcast(f"Client#{client_id} says: {data}", author=websocket)
-
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client#{client_id} has left the chat", author=websocket)
-
-
 
 
 if __name__ == "__main__":
