0831bd398a7d9776f9b841ccced1ec2bcd2f7e62
Fedor <feodor.a.v.03@gmail.com>
2025-05-26 09:40:40 +0300

    fix redis

--- app/api_v1/auth/view.py
+++ app/api_v1/auth/view.py
@@ -189,8 +189,8 @@ async def auth_user_issue_jwt(
     logger.info(f"Code: {code}")
     async with redis as r:
         user_id = await r.get(f"auth_code:{code}")
+    user_id = user_id.decode("utf-8")
     logger.info(f"Get user_id from redis code {user_id}")
-
     user = await users_service.get_user_by_id(session=session, id=int(user_id))
     access_token = await setup_access_token(user=user)
     refresh_token = await setup_refresh_token(user=user)
