cc87da7a2eef8aa0f1c6e9e8eea5eca16595416b
Fedor <feodor.a.v.03@gmail.com>
2025-05-29 21:14:05 +0300

    ff

--- app/api_v1/assistant_messages/models.py
+++ app/api_v1/assistant_messages/models.py
@@ -2,7 +2,7 @@ from app.core.base.base_model import Base
 from app.api_v1.users.mixins import UsersRelationMixin
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 import enum
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, ForeignKey, Text
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
 class AssistantMessagesOrm(UsersRelationMixin, Base):
     __tablename__ = "assistant_messages"
     _user_back_populates = "assistant_messages"
-    text: Mapped[str] = mapped_column(String(5000))
+    text: Mapped[str] = mapped_column(Text)
     type: Mapped[str] = mapped_column(nullable=False)
     assistant_chat_id: Mapped[int] = mapped_column(ForeignKey("assistant_chats.id", ondelete="CASCADE"))
     assistant_chat: Mapped["AssistantChatsOrm"] = relationship(back_populates="messages")
